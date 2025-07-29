a22e78bd80dd8b974f595f6379a1d9c3f4f5d02e
Momen <momen.devv@gmail.com>
2025-07-29 07:45:31 +0300

    chore: add morgan HTTP request logger middleware for development debugging

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,12 @@ import AppError from './utils/appError';
 import userRoutes from './routes/user.routes';
 import HttpStatusCodes from './utils/HttpStatusCodes';
 import errorHandler from './middlewares/globalErrorHandler';
+import morgan from 'morgan';
+import configKeys from './config';
 
 const app: Application = express();
+
+if (configKeys.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
 
@@ -15,6 +19,7 @@ app.all(/(.*)/, (req, res, next: NextFunction) => {
   next(new AppError('Not found', HttpStatusCodes.NOT_FOUND));
 });
 
+//* handles server side errors
 app.use(errorHandler);
 
 export default app;
