b4016f9457ab68e8f6741b81c1735eeaa83e3f88
Momen <momen.devv@gmail.com>
2025-07-29 06:39:11 +0300

    feat: add initial user route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express, { Application, NextFunction } from 'express';
 import AppError from './utils/appError';
+import userRoutes from './routes/user.routes';
+import HttpStatusCodes from './utils/HttpStatusCodes';
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.get('*', (req, res, next: NextFunction) => {
-  next(new AppError('Not found', 404));
+app.use('/api/v1/users', userRoutes);
+
+//* catch 404 and forward to error handler
+app.all(/(.*)/, (req, res, next: NextFunction) => {
+  next(new AppError('Not found', HttpStatusCodes.NOT_FOUND));
 });
 
 export default app;
