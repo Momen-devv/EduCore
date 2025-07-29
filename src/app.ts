1f68e9d3611514d9721d421216d6a0491950eee4
Momen <momen.devv@gmail.com>
2025-07-29 03:55:01 +0300

    initialize TypeScript + Express project

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express,{Request,Response}  from "express";
+
+const app = express ()
+
+app.use(express.json());
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send({message:'Hello!!'})
+})
+
+app.listen(3000, () => {
+    console.log(`The server is running at http://localhost:3000`);
+});
